8086a884001a0e25f8b9f0376d6a5f0b6911c83b
DeepakYadavGit24k <deepakyadaviitg@gmail.com>
2024-08-29 19:04:50 +0000

    datatype-summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,17 +1,19 @@
 
 // try with null, undefined, true, false, string
 
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 // "33" => 33
 // "33abc" => Nan
 // true => 1; false => 0
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-let isLoogedIn = 1
-let booleanIsLoggedIn = Boolean(booleanIsLoggedIn)
+// let isLoogedIn = 1
+// let booleanIsLoggedIn = Boolean(booleanIsLoggedIn)
+
+// operations
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,7 @@
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+// strick check
+// Ifn this case data type is also checked
+console.log("2"=== 2)
